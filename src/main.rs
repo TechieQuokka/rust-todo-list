8018a811cd6be4f9157717b5fc6d96e80e657787
Beethoven <everestfesf@gmail.com>
2025-09-15 18:01:23 +0900

    todo.rs 작업 완료

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello World!!");
+use models::todo::{Todo, Priority};
+use std::thread::sleep;
+use std::time::Duration;
+
+mod models {
+    pub mod todo;
+}
+
+fn main () {
+
+    println!("test start!");
+
+    let mut todo = Todo::new("Rust Study!".to_string());
+    println!("Todo : {:?}", todo);
+
+    sleep(Duration::from_secs(2));
+
+    todo.complete();
+    println!("Todo : {:?}", todo);
+
+    todo.update_title("Rust Wow!!!".to_string());
+    println!("Todo : {:?}", todo);
+
+    println!("priority : {:?}", todo.priority);
 }
---
+++ src/models/todo.rs
@@ -0,0 +1,50 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use uuid::Uuid;
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub struct Todo {
+
+  pub id: Uuid,
+  pub title: String,
+  pub description: Option<String>,
+  pub completed: bool,
+  pub created_at: DateTime<Utc>,
+  pub updated_at: DateTime<Utc>,
+  pub priority: Priority,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+pub enum Priority {
+
+  Low, Medium, High
+}
+
+impl Todo {
+  pub fn new(title: String) -> Self {
+
+    let now = Utc::now();
+    Self {
+
+      id: Uuid::new_v4(),
+      title,
+      description: None,
+      completed: false,
+      created_at: now,
+      updated_at: now,
+      priority: Priority::Medium,
+    }
+  }
+
+  pub fn complete (&mut self) {
+
+    self.completed = true;
+    self.updated_at = Utc::now();
+  }
+
+  pub fn update_title (&mut self, title: String) {
+
+    self.title = title;
+    self.updated_at = Utc::now();
+  }
+}
